73c3e326ca87c1d97804737374b5bdf015a12e08
Thomas Brezinski <thomas.brezinski@wpengine.com>
2015-03-14 22:33:28 -0500

    Add barebones project 1 sketch file

---
+++ project_1/project_1.ino
@@ -0,0 +1,11 @@
+// project 1: basic digital logic
+
+// the setup function runs once when you press reset or power the board
+// It should be used to initialize the hardware you way you would like it
+// prior to your main program running
+void setup() {
+}
+
+// the loop functions runs over and over again forever
+void loop() {
+}
