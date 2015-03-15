6a932be0b08c9eb0692c6dcea6efcd94687c6b47
Thomas Brezinski <thomas.brezinski@wpengine.com>
2015-03-15 00:11:23 -0500

    Initalize project 1 GPIO

--- project_1/project_1.ino
+++ project_1/project_1.ino
@@ -4,6 +4,12 @@
 // It should be used to initialize the hardware you way you would like it
 // prior to your main program running
 void setup() {
+  // initialize digital pins 9 and 10 as inputs.
+  pinMode(9, INPUT);
+  pinMode(10, INPUT);
+  // initialize digital pins 11 and 12 as outputs.
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 // the loop functions runs over and over again forever
